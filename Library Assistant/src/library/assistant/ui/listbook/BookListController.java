9596fddea8041e267366a67ad1d3b679431faa5a
Amit <Amit@LENOVO-PC>
2018-02-06 16:30:43 +0530

    listBook

---
+++ Library Assistant/src/library/assistant/ui/listbook/BookListController.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library.assistant.ui.listbook;
+
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import library.assistant.database.DatabaseHandler;
+
+/**
+ * FXML Controller class
+ *
+ * @author Amit
+ */
+public class BookListController implements Initializable {
+
+    @FXML
+    private AnchorPane rootPane;
+    @FXML
+    private TableView<Book> tableView;
+    @FXML
+    private TableColumn<Book, String> titleCol;
+    @FXML
+    private TableColumn<Book, String> idCol;
+    @FXML
+    private TableColumn<Book, String> authorCol;
+    @FXML
+    private TableColumn<Book, String> publisherCol;
+    @FXML
+    private TableColumn<Book, Boolean> availabilityCol;
+
+    ObservableList<Book> list = FXCollections.observableArrayList();
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        initCol();
+        loadData();
+    }
+
+    private void initCol() {
+        titleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
+        idCol.setCellValueFactory(new PropertyValueFactory("id"));
+        authorCol.setCellValueFactory(new PropertyValueFactory<>("author"));
+        publisherCol.setCellValueFactory(new PropertyValueFactory("publisher"));
+        availabilityCol.setCellValueFactory(new PropertyValueFactory("availability"));
+
+    }
+
+    private void loadData() {
+        try {
+            DatabaseHandler handler = new DatabaseHandler();
+            String query = "SELECT * FROM BOOK";
+            ResultSet rs = handler.execQuery(query);
+            while (rs.next()) {
+                String gtitle = rs.getString("title");
+                String gid = rs.getString("id");
+                String gauthor = rs.getString("author");
+                String gpublisher = rs.getString("publisher");
+                Boolean gavailability = rs.getBoolean("isAvail");
+                list.add(new Book(gtitle,gid,gauthor,gpublisher,gavailability)); 
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(BookListController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        tableView.getItems().setAll(list);
+    }
+
+    public static class Book {
+
+        private final SimpleStringProperty title;
+        private final SimpleStringProperty id;
+        private final SimpleStringProperty author;
+        private final SimpleStringProperty publisher;
+        private final SimpleBooleanProperty availability;
+
+        Book(String title, String id, String author, String publisher, Boolean availability) {
+            this.title = new SimpleStringProperty(title);
+            this.id = new SimpleStringProperty(id);
+            this.author = new SimpleStringProperty(author);
+            this.publisher = new SimpleStringProperty(publisher);
+            this.availability = new SimpleBooleanProperty(availability);
+        }
+
+        public String getTitle() {
+            return title.get();
+        }
+
+        public String getId() {
+            return id.get();
+        }
+
+        public String getAuthor() {
+            return author.get();
+        }
+
+        public String getPublisher() {
+            return publisher.get();
+        }
+
+        public Boolean getAvailability() {
+            return availability.get();
+        }
+
+    }
+
+}
